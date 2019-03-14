edf1afd0b4fec5e93bb6a9d5a0734e58d56dcc75
Shahed Rahim <shahed21@gmail.com>
2019-03-14 00:03:21 -0500

    Initial Commit

---
+++ MineSweeper/src/com/shahedrahim/minesweeper/Board.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shahedrahim.minesweeper;
+
+/**
+ * @author shahe
+ *
+ */
+class Board {
+	private static String LOG = "Board";
+	
+	public static final int ROWS = 10;
+	public static final int COLS = 10;
+	public static final int BOARDSIZE = 100;
+	
+	
+	private boolean bombs[BOARDSIZE];
+
+}
---
+++ MineSweeper/src/com/shahedrahim/minesweeper/MineSweeper.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shahedrahim.minesweeper;
+
+/**
+ * @author shahe
+ *
+ */
+public class MineSweeper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
